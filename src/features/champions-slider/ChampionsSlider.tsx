ea2e961f803fcb461b1253f147dd10c712732cc6
Nataly Baranova <borashek@inbox.ru>
2024-06-09 22:25:53 +0300

    mobile

--- src/features/champions-slider/ChampionsSlider.tsx
+++ src/features/champions-slider/ChampionsSlider.tsx
@@ -17,9 +17,8 @@ export const ChampionsSlider = () => {
     dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
+    slidesToShow: 3,
+    slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
@@ -39,13 +38,6 @@ export const ChampionsSlider = () => {
           dots: true
         }
       },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
     ]
   }
 
